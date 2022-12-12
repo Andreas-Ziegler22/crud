39cc548c486911533641e09cac38e0f241f59b9a
MrBurp <andreass.ziegler@gmail.com>
2022-12-12 11:18:56 +0100

    nav componet updated

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -1,4 +1,15 @@
 import "./Nav.css";
 import React from "react";
 
-export default (props) => <aside className="menu-area">Navegation</aside>;
+export default (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+      <a href="#/">
+        <i className="fa fa-home"></i> Home
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users">User</i>
+      </a>
+    </nav>
+  </aside>
+);
