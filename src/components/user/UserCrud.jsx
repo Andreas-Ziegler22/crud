07385d9a799466d91000238172a11428e7cedf4d
MrBurp <andreass.ziegler@gmail.com>
2022-12-12 12:37:10 +0100

    pre user component

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -1,2 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import Main from "../template/Main";
+
+const headerProps = {
+  icon: "users",
+  title: "Users",
+  subtitle: "create user: crud",
+};
+
+export default class Usercrud extends Component {
+  render() {
+    return <Main {...headerProps}>Users</Main>;
+  }
+}
