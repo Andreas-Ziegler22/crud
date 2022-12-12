574d7711ed26878fec4ecdddff7ed1bad48fd2de
MrBurp <andreass.ziegler@gmail.com>
2022-12-12 12:23:00 +0100

    updated main to home component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => (
+  <Main icon="home" title="Home" subtitle="Possible subtitle">
+    <div className="display-4">Welcome!</div>
+    <p className="mb-0">CRUD - create write update delete in React!</p>
+  </Main>
+);
