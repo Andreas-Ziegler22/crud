af881a24a248e53aae965b8509118e7b247c3264
MrBurp <andreass.ziegler@gmail.com>
2022-12-12 11:03:52 +0100

    footer styled

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -1,4 +1,10 @@
 import "./Footer.css";
 import React from "react";
 
-export default (props) => <footer className="footer">Footer</footer>;
+export default (props) => (
+  <footer className="footer">
+    <span>
+      Developed by Andreas <i className="fa fa-smile-o text-danger"></i>
+    </span>
+  </footer>
+);
