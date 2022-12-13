cf2a4957fc7fcb55ac802316bcd30fe3ff0fd068
MrBurp <andreass.ziegler@gmail.com>
2022-12-13 20:46:53 +0100

    load update the user

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -99,6 +99,10 @@ export default class Usercrud extends Component {
     );
   }
 
+  load(user) {
+    this.setState({ user });
+  }
+
   render() {
     return <Main {...headerProps}>{this.renderForm()}</Main>;
   }
