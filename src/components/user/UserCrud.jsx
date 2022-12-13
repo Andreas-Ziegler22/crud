ce9dc3b3b687c798d8c4dc907493482d421e4a47
MrBurp <andreass.ziegler@gmail.com>
2022-12-13 17:58:29 +0100

    save user or change user

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -21,6 +21,22 @@ export default class Usercrud extends Component {
     this.setState({ user: initialState.user });
   }
 
+  save() {
+    const user = this.setState.user;
+    const method = user.id ? "put" : "post";
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+    axios[method](url, user).then((resp) => {
+      const list = this.getUpdatedList(resp.data);
+      this.setState({ user: initialState.user, list });
+    });
+  }
+
+  getUpdatedList(user) {
+    const list = this.state.list.filter((u) => u.id !== user.id);
+    list.unshift(user);
+    return list;
+  }
+
   render() {
     return <Main {...headerProps}>Users</Main>;
   }
